7d9c829ba59fee1482bc9fb9ee1c95b89d3449d7
[Ruslan] <[rkutaraev@list.ru]>
2024-03-16 12:09:04 +0300

    code refactoring 2

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import VueCarousel from 'vue-carousel'
 
-createApp(App).mount('#app')
+Vue.use(VueCarousel)
+
+Vue.config.productionTip = false
+
+new Vue({
+  store,
+  render: h => h(App)
+}).$mount('#app')
