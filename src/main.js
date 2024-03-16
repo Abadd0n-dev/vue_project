0bf8c7851beeb42b064aaf339c9ca9d5986d3eab
[Ruslan] <[rkutaraev@list.ru]>
2024-03-16 15:00:45 +0300

    code refactoring 3

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
-import VueCarousel from 'vue-carousel'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VueCarousel from "vue-carousel";
 
-Vue.use(VueCarousel)
+Vue.use(VueCarousel);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
+  router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
